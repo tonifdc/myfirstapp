6b012a87abc8a79c6fb9972a4c1f498a61a6c94d
Federico DC <dc.federico@gmail.com>
2019-04-29 20:58:55 -0300

    practica

---
+++ src/components/Navigation.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class Navigation extends Component {
+
+  render() {   
+    return ( 
+        
+    <nav, className="navbar navbar-dark bg-dark">
+    <a href="" className="text-white">
+      { "hello".toUpperCase}
+    </a>
+ </nav> 
+ ) }
+}
+export default Navigation;
